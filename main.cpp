930a7ac48f1234574ac468fcc1a533edc32ca18b
jniemann66 <jniemann666@bigpond.com>
2019-09-29 21:40:11 +1000

    set up loop over multiple input files

--- main.cpp
+++ main.cpp
@@ -12,28 +12,35 @@ int main(int argc, char** argv)
 	(void)argc; // unused
 	(void)argv; // unused
 
+	// acquire parameters
+	// todo: from args
 	Sndspec::Parameters parameters;
-	parameters.setInputFiles({"/tmp/testfile.wav"});
+	parameters.setInputFiles({"/tmp/testfile.wav", "/tmp/guitar.flac"});
+	parameters.setFftSize(512);
 
+	// make a suitable Kaiser window
 	Sndspec::KaiserWindow<double> k;
 	k.setBeta(Sndspec::KaiserWindow<double>::betaFromDecibels(parameters.getDynRange()));
 	k.generate(parameters.getFftSize());
 
-	Sndspec::Reader<double> r(parameters.getInputFiles().at(0), [](int64_t pos, int ch, const double* data) -> void{
-		(void)data; // unused
-		std::cout << "pos " << pos << " ch " << ch << std::endl;
-	}, parameters.getFftSize(), parameters.getImgWidth());
+	for(const std::string& inputFilename : parameters.getInputFiles()) {
+		std::cout << "Opening input file: " << inputFilename << std::endl;
+		Sndspec::Reader<double> r(inputFilename, [](int64_t pos, int ch, const double* data) -> void {
+			(void)data; // unused
+			std::cout << "pos " << pos << " ch " << ch << std::endl;
+		}, parameters.getFftSize(), parameters.getImgWidth());
 
-	if(r.getSndFileHandle() == nullptr) {
-		std::cout << "problem opening file" << std::endl;
-	} else {
-		r.setWindow(k.getData());
-		std::vector<std::unique_ptr<double[]>> channelBuffers;
-		for(int ch = 0; ch < r.getNChannels(); ch ++) {
-			double* p = new double[static_cast<size_t>(parameters.getFftSize())];
-			channelBuffers.emplace_back(p);
-			r.setChannelBuffer(ch, p);
+		if(r.getSndFileHandle() == nullptr) {
+			std::cout << "problem opening file" << std::endl;
+		} else {
+			r.setWindow(k.getData());
+			std::vector<std::unique_ptr<double[]>> channelBuffers;
+			for(int ch = 0; ch < r.getNChannels(); ch ++) {
+				double* p = new double[static_cast<size_t>(parameters.getFftSize())];
+				channelBuffers.emplace_back(p);
+				r.setChannelBuffer(ch, p);
+			}
+			r.read();
 		}
-		r.read();
 	}
 }
