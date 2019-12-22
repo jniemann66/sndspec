7460b8206b59ff39fedcd66e26156fb5608ac4d2
Judd Niemann <jniemann666@bigpond.com>
2019-12-22 18:28:35 +1100

    cleanup

--- tests.cpp
+++ tests.cpp
@@ -5,7 +5,6 @@ bool tests::testWindow()
 {
 	Sndspec::Window<double> w;
 	w.generate("triangular", 9, 20);
-//	w.generateWindow1(8);
 	for(double v : w.getData())
 	{
 		std::cout << v << std::endl;
