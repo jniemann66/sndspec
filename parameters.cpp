444d0eb69b24c3ff368c3681621ec0be374062b3
jniemann66 <jniemann666@bigpond.com>
2020-01-15 21:52:00 +1100

    clean up

--- parameters.cpp
+++ parameters.cpp
@@ -22,6 +22,9 @@
 
 namespace Sndspec {
 
+constexpr int minImgWidth = 160;
+constexpr int minImgHeight = 160;
+
 std::vector<std::string> Parameters::getInputFiles() const
 {
 	return inputFiles;
@@ -110,13 +113,13 @@ std::string Parameters::fromArgs(const std::vector<std::string> &args)
 			break;
 		case Height:
 			if(++argsIt != args.cend()) {
-				imgHeight = std::max(160, std::stoi(*argsIt));
+                imgHeight = std::max(minImgHeight, std::stoi(*argsIt));
 				++argsIt;	
 			}
 			break;
 		case Width:
 			if(++argsIt != args.cend()) {
-				imgWidth = std::max(160, std::stoi(*argsIt));
+                imgWidth = std::max(minImgWidth, std::stoi(*argsIt));
 				++argsIt;	
 			}
 			break;
